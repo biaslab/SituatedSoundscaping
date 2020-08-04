589d4a1518f5dcb7b1fe2ad2ddf5cedde50838c1
Erp <b.v.erp@student.tue.nl>
2020-08-04 14:51:33 +0200

    ⚙️ update io file to export train and test signals

--- realtime/src/io.jl
+++ realtime/src/io.jl
@@ -2,26 +2,32 @@ using WAV: wavread
 using DSP: resample
 
 
-function load_data(filenames::Array{String,1}; fs::Real=16000, duration::Real=1, levels_dB::Union{Array{Int64,1}, Array{Float64,1}}=zeros(length(filenames)), subtract_mean::Bool=true, normalize_std::Bool=true)
+function load_data(filenames::Array{String,1}; fs::Real=16000, duration::Union{Array{Float64,1}, Array{Int64,1}}=[1,1], levels_dB::Union{Array{Int64,1}, Array{Float64,1}}=zeros(length(filenames)), subtract_mean::Bool=true, normalize_std::Bool=true)
 
     # create array for data files
-    y = Array{Array{Float64,1},1}(undef, length(filenames))
+    y_train = Array{Array{Float64,1},1}(undef, length(filenames))
+    y_test = Array{Array{Float64,1},1}(undef, length(filenames))
 
-    ## loop through different files
+    # loop through different files
     for (index, filename) in enumerate(filenames)
 
-        ## load file (first load sampling frequency to prevent loading all data)
+        # load file (first load sampling frequency to prevent loading all data)
         _, fs_tmp = wavread(filename; subrange=1)
         fs_tmp = convert(Float64, fs_tmp)
-        y_tmp, _ = wavread(filename; subrange=fs_tmp*duration)
+        y_tmp, _ = wavread(filename; subrange=fs_tmp*maximum(duration))
         y_tmp = y_tmp[:,1]
         y_tmp = squeeze(y_tmp)
 
-        ## preprocess data
-        y[index] = preprocess(y_tmp, fs_tmp; fs=fs, duration=duration, level_dB=levels_dB[index], subtract_mean=subtract_mean, normalize_std=normalize_std)
-
+        # preprocess data
+        if duration[1] > duration[2]
+            y_train[index] = preprocess(y_tmp, fs_tmp; fs=fs, duration=duration[1], level_dB=levels_dB[index], subtract_mean=subtract_mean, normalize_std=normalize_std)
+            y_test[index] = y_train[index][1:duration[2]*fs]
+        else
+            y_test[index] = preprocess(y_tmp, fs_tmp; fs=fs, duration=duration[2], level_dB=levels_dB[index], subtract_mean=subtract_mean, normalize_std=normalize_std)
+            y_train[index] = y_test[index][1:duration[1]*fs]        
+        end
     end
 
-    return y
+    return y_train, y_test
 
 end
