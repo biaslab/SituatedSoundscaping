dabc5105e05d24bbb95bba2d30e7a90d044a2792
Bart van Erp <b.v.erp@tue.nl>
2021-03-31 12:52:58 +0200

    convert to package

---
+++ src/SituatedSoundscaping.jl
@@ -0,0 +1,15 @@
+__precompile__()
+
+module SituatedSoundscaping
+
+    using PyCall
+    
+    # present the package to the user
+    @info "================================="
+    @info "===   SITUATED SOUNDSCAPING   ==="
+    @info "================================="
+
+    
+
+end # module
+
