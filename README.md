cda1d94260bbc662c368803fb6618226700b28a6
Bart van Erp <b.v.erp@tue.nl>
2021-04-13 16:41:01 +0200

    include mixture preparation

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ This repository will contain the most up-to-date progress of the Situated Sounds
 
 The Situated Soundscaping project is focussed towards enabling hearing aid users to be in control of their hearing aid algorithm. The user should be able to record a short fragment of the sound that he or she perceives and decide whether this sound needs to be amplified or surpressed. 
 
-The speech data set used for this project has been retrieved from https://www.openslr.org/12 and represents 100 hours of cleaned speech recordings. The noise data set has been obtained from http://web.cse.ohio-state.edu/pnl/corpus/HuNonspeech/HuCorpus.html and holds 100 recordings.
+The speech data set used for this project has been retrieved from https://www.openslr.org/12 and represents 100 hours of cleaned speech recordings. The noise data set has been obtained from http://www.eduardofonseca.net/FSDnoisy18k/#download.
--- src/data/prepare_data.jl
+++ src/data/prepare_data.jl
@@ -3,7 +3,35 @@ using DSP
 using ProgressMeter
 using HDF5
 
-export prepare_data, read_recording
+export prepare_data, read_recording, create_mixture_signal
+
+function create_mixture_signal(filename_speech::String, filename_noise::String; duration_adapt::Int64=3, duration_test::Int64=10, fs::Int64=16000, block_length::Int64=64)
+
+    # load and preprocess speech signal
+    x_speech, fs_speech = load(filename_speech)
+    x_speech = squeeze(x_speech)
+    x_speech = resample(x_speech, fs/fs_speech)
+    x_speech .+= 1e-5*randn(length(x_speech))
+    x_speech .-= mean(x_speech)
+    x_speech ./= std(x_speech)
+    x_speech = x_speech[fs*duration_adapt+1:fs*(duration_adapt+duration_test)]
+
+    # load and preprocess noise signal
+    x_noise, fs_noise = load(filename_noise)
+    x_noise = squeeze(x_noise)
+    x_noise = resample(x_noise, fs/fs_noise)
+    x_noise .+= 1e-5*randn(length(x_noise))
+    x_noise .-= mean(x_noise)
+    x_noise ./= std(x_noise)
+    x_noise = x_noise[fs*duration_adapt+1:fs*(duration_adapt+duration_test)]
+
+    # create mixture signal
+    x_mixture = x_speech + x_noise
+
+    # return signals
+    return x_mixture, x_speech, x_noise
+
+end
 
 function read_recording(filename::String; duration::Real=3, fs::Int64=16000, block_length::Int64=64)
     X = h5read(filename, "data_complex")
@@ -57,6 +85,7 @@ function prepare_data(input_folder::String, output_folder::String; block_length:
             x_tmp = resample(x_tmp, fs/fs_tmp)
             x_tmp .+= 1e-5*randn(length(x_tmp))
             x_tmp .-= mean(x_tmp)
+            x_tmp ./= std(x_tmp)
 
             # convert to spectrum
             Xs = Array{Complex{Float64},2}(undef, Int(block_length/2+1), Int(floor(length(x_tmp)/block_length)))
