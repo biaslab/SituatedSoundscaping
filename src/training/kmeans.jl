e6671de0e70f3e49e2f2478253caa4f5293c4adb
Bart van Erp <b.v.erp@tue.nl>
2021-05-06 10:34:36 +0200

    generalize kmeans and em functions

--- src/training/kmeans.jl
+++ src/training/kmeans.jl
@@ -42,3 +42,44 @@ function train_kmeans(model_name::String, data::Data, nr_mixtures::Int64)
     return centers, πk
 
 end
+
+
+function train_kmeans(model_name::String, data::Array{Float64,2}, nr_mixtures::Int64)
+
+    # fetch dimensions 
+    nr_frequencies = size(data,1)
+
+    # filename
+    filename = model_name*"_"*string(nr_frequencies)*"_"*string(nr_mixtures)*"_1.h5"
+
+    # check if model exists
+    if isfile(filename)
+
+        @info "Kmeans model already trained."
+
+        # load model 
+        f = h5open(filename, "r")
+        centers = HDF5.read(f["centers"]);
+        πk = HDF5.read(f["assignments"]);
+        close(f)
+
+    else
+
+        # train model
+        km = ParallelKMeans.kmeans(data, nr_mixtures)
+        
+        # extract parameters
+        centers = km.centers;
+        πk = normalize_sum([count(x->x==k, km.assignments) for k in 1:nr_mixtures]);
+
+        # save model
+        f = h5open(filename, "w")
+        HDF5.write(f, "centers", centers);
+        HDF5.write(f, "assignments", πk);
+        close(f)
+        
+    end 
+
+    return centers, πk
+
+end
