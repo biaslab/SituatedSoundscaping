a0bb29a183e4cf84579823541cd03d9bb69842c6
Bart van Erp <b.v.erp@tue.nl>
2021-05-06 10:33:55 +0200

    add algonquin algorithm

---
+++ src/separation/source_separation_algonquin.jl
@@ -0,0 +1,163 @@
+export separate_sources_algonquin
+
+function separate_sources_algonquin(x, means_speech, covs_speech, w_speech, means_noise, covs_noise, w_noise; block_length::Int64=64, fs::Int64=16000, observation_noise_precision::Float64=1e5)
+
+    # calculate number of blocks to process
+    nr_blocks = Int(length(x)/block_length)
+
+    # initialize warped filter bank
+    filterbank = warped_filter_bank(block_duration_s=block_length/16000, nr_bands=Int(block_length/2)+1)
+
+    # allocate some space
+    X = Array{Complex{Float64},1}(undef, Int(block_length/2)+1)
+    G = Array{Float64,2}(undef, nr_blocks, Int(block_length/2)+1)
+    output = zeros(size(x))
+
+    # loop through blocks
+    @showprogress for n in 1:nr_blocks
+
+        # feed signal into filterbank
+        run!(filterbank, x[1+(n-1)*block_length:n*block_length])
+
+        # extract log-power  coefficients
+        X = log.(abs2.(squeeze(get_frequency_coefficients(filterbank))))
+
+        # calculate weighted gain
+        G[n,:] = loop_inference_algonquin(X, means_speech, covs_speech, w_speech, means_noise, covs_noise, w_noise, observation_noise_precision, block_length)
+
+        # update filter fir_weights
+        update_fir_weights!(filterbank, G[n,:])
+
+        # read from filter
+        output[1+(n-1)*block_length:n*block_length] = read(filterbank)
+
+    end
+
+    return output, G
+
+end
+
+# perform inference for all possible combinations
+function loop_inference_algonquin(data, means_speech, covs_speech, w_speech, means_noise, covs_noise, w_noise, observation_noise_precision, block_length)
+
+    # fetch items 
+    nr_mixtures_speech = size(means_speech,2)
+    nr_mixtures_noise = size(means_noise,2)
+
+    # allocate space for free energy and gain vectors
+    score = Array{Float64,2}(undef, nr_mixtures_speech, nr_mixtures_noise)
+    G = Array{Array{Float64,1},2}(undef, nr_mixtures_speech, nr_mixtures_noise)
+
+    # loop over combinations
+    for (ids, idn) in Iterators.product(1:nr_mixtures_speech, 1:nr_mixtures_noise)
+
+        # do inference
+        score[ids, idn], G[ids, idn] = inference_algonquin(data, means_speech[:,ids], covs_speech[:,ids], means_noise[:,idn], covs_noise[:,idn], observation_noise_precision, block_length)
+
+    end
+
+    # determine posterior mixtures
+    prior = w_speech * w_noise'
+    @assert sum(prior) ≈ 1
+    posterior = score .+ log.(prior)
+    softmax!(posterior)
+    
+    # weight gain by posteriors
+    Gw = sum(G .* posterior)
+
+    # return weighted gain
+    return Gw
+
+end
+
+function inference_algonquin(data, mean_speech, cov_speech, mean_noise, cov_noise, observation_noise_precision, block_length)
+    x = mean_speech
+    n = mean_noise
+    Φ = vcat(cov_speech, cov_noise)
+    Σ = vcat(cov_speech, cov_noise)
+    Ψ = ones(Int(block_length/2+1))/observation_noise_precision
+    μ = vcat(mean_speech, mean_noise)
+    y = data
+    
+    gi = zeros(Int(block_length/2+1))
+    gi_derivative = zeros(Int(block_length+2))
+    
+    g!(gi, x, n, block_length)
+    g_derivative!(gi_derivative, x, n, block_length)
+    
+    for it = 1:5
+        update!(y, gi, gi_derivative, Φ, x, n, μ, Σ, Ψ, block_length)
+    end
+    # calculate gain
+    G = exp.(x) ./ (exp.(x) + exp.(n))
+
+    # score model
+    score = score_algonquin(y, gi, gi_derivative, Φ, x, n, μ, Σ, Ψ, block_length)
+
+    # return
+    return score, G
+end
+
+function g!(gi::Array{Float64,1}, x::Array{Float64,1}, n::Array{Float64,1}, block_length::Int64)
+    @inbounds for k = 1:Int(block_length/2+1)
+        gi[k] = x[k] + log(1 + exp(n[k]-x[k]))
+    end
+end
+
+function g_derivative!(gi_derivative::Array{Float64,1}, x::Array{Float64,1}, n::Array{Float64,1}, block_length::Int64)
+    @inbounds for k = 1:Int(block_length/2+1)
+        tmp = exp(n[k]-x[k])
+        gi_derivative[k] = 1 / (1 + tmp)
+        gi_derivative[k+Int(block_length/2+1)] = gi_derivative[k] * tmp
+    end
+end
+
+function update!(y::Array{Float64,1}, gi::Array{Float64,1}, gi_derivative::Array{Float64,1}, Φ::Array{Float64,1}, x::Array{Float64,1}, n::Array{Float64,1}, μ::Array{Float64,1}, Σ::Array{Float64,1}, Ψ::Array{Float64,1}, block_length::Int64)
+    # update estimate for y
+    g!(gi, x, n, block_length)
+
+    # update derivative in point
+    g_derivative!(gi_derivative, x, n, block_length)
+
+    # update points
+    for k = 1:Int(block_length/2+1)
+
+        # speech elements
+        Φ[k] = 1 / (1 / Σ[k] + gi_derivative[k]^2 / Ψ[k] )
+        x[k] += Φ[k]*((μ[k] - x[k])/Σ[k] + gi_derivative[k]*(y[k] - gi[k])/Ψ[k])
+
+        # noise elements
+        Φ[k+Int(block_length/2+1)] = 1 / (1 / Σ[k+Int(block_length/2+1)] + gi_derivative[k+Int(block_length/2+1)]^2 / Ψ[k] )
+        n[k] += Φ[k+Int(block_length/2+1)]*((μ[k+Int(block_length/2+1)] - n[k])/Σ[k+Int(block_length/2+1)] + gi_derivative[k+Int(block_length/2+1)]*(y[k] - gi[k]) / Ψ[k])
+
+    end
+
+end
+
+function score_algonquin(y::Array{Float64,1}, gi::Array{Float64,1}, gi_derivative::Array{Float64,1}, Φ::Array{Float64,1}, x::Array{Float64,1}, n::Array{Float64,1}, μ::Array{Float64,1}, Σ::Array{Float64,1}, Ψ::Array{Float64,1}, block_length::Int64)
+    
+    # update estimate for y
+    g!(gi, x, n, block_length)
+
+    # update derivative in point
+    g_derivative!(gi_derivative, x, n, block_length)
+
+    # calculate temporary term
+    tmp1 = 0
+    tmp2 = 0
+    for k = 1:Int(block_length/2+1)
+        tmp1 += (x[k]-μ[k])^2/Σ[k]
+        tmp1 += (n[k]-μ[k+Int(block_length/2+1)])^2/Σ[k+Int(block_length/2+1)]
+
+        tmp2 += gi_derivative[k]^2/Ψ[k]*Φ[k]
+        tmp2 += gi_derivative[k+Int(block_length/2+1)]^2/Ψ[k]*Φ[k+Int(block_length/2+1)]
+    end
+
+    return -0.5*sum(log.(Σ)) +
+            0.5*sum(log.(Φ)) - 
+            0.5*sum((y - gi).^2 ./ Ψ) -
+            0.5*sum(Φ ./ Σ) -
+            0.5*tmp1 - 
+            0.5*tmp2
+
+end
