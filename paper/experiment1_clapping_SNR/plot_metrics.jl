abc1ae794d9d7fa7ff1c6b480355ccc230f0df90
Bart van Erp <b.v.erp@tue.nl>
2021-06-15 14:01:22 +0200

    Converted to the PGFPlots-ism

---
+++ paper/experiment1_clapping_SNR/plot_metrics.jl
@@ -0,0 +1,126 @@
+using HDF5
+using PGFPlotsX
+
+
+# fetch results
+begin
+    metrics_algonquin = Dict("SNR"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "PESQ_nb"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "PESQ_wb"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "STOI"=> Dict("x"=> zeros(5), "y"=> zeros(5)))
+    metrics_baseline = Dict("SNR"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "PESQ_nb"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "PESQ_wb"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "STOI"=> Dict("x"=> zeros(5), "y"=> zeros(5)))
+    for metrics_file in filter(x -> occursin(".h5", x) & occursin("metrics",x), readdir("paper/experiment1_clapping_SNR/exports/algonquin_vb",join=true))
+        SNR = -parse(Int64, split(metrics_file[findfirst("power=", metrics_file)[end]+1:end], ".")[1])
+        metrics_algonquin["SNR"]["y"][SNR÷5+3] = h5read(metrics_file, "new_SNR")
+        metrics_algonquin["SNR"]["x"][SNR÷5+3] = SNR
+        metrics_algonquin["PESQ_nb"]["y"][SNR÷5+3] = h5read(metrics_file, "new_PESQnb")
+        metrics_algonquin["PESQ_nb"]["x"][SNR÷5+3] = SNR
+        metrics_algonquin["PESQ_wb"]["y"][SNR÷5+3] = h5read(metrics_file, "new_PESQwb")
+        metrics_algonquin["PESQ_wb"]["x"][SNR÷5+3] = SNR
+        metrics_algonquin["STOI"]["y"][SNR÷5+3] = h5read(metrics_file, "new_STOI")
+        metrics_algonquin["STOI"]["x"][SNR÷5+3] = SNR
+        metrics_baseline["SNR"]["y"][SNR÷5+3] = h5read(metrics_file, "baseline_SNR")
+        metrics_baseline["SNR"]["x"][SNR÷5+3] = SNR
+        metrics_baseline["PESQ_nb"]["y"][SNR÷5+3] = h5read(metrics_file, "baseline_PESQnb")
+        metrics_baseline["PESQ_nb"]["x"][SNR÷5+3] = SNR
+        metrics_baseline["PESQ_wb"]["y"][SNR÷5+3] = h5read(metrics_file, "baseline_PESQwb")
+        metrics_baseline["PESQ_wb"]["x"][SNR÷5+3] = SNR
+        metrics_baseline["STOI"]["y"][SNR÷5+3] = h5read(metrics_file, "baseline_STOI")
+        metrics_baseline["STOI"]["x"][SNR÷5+3] = SNR
+    end
+
+    # fetch metrics gs_sum
+    metrics_gs_sum = Dict("SNR"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "PESQ_nb"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "PESQ_wb"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "STOI"=> Dict("x"=> zeros(5), "y"=> zeros(5)))
+    for metrics_file in filter(x -> occursin(".h5", a), readdir("paper/experiment1_clapping_SNR/exports/gs_sum",join=true))
+        SNR = -parse(Int64, split(metrics_file[findfirst("power=", metrics_file)[end]+1:end], ".")[1])
+        metrics_gs_sum["SNR"]["y"][SNR÷5+3] = h5read(metrics_file, "new_SNR")
+        metrics_gs_sum["SNR"]["x"][SNR÷5+3] = SNR
+        metrics_gs_sum["PESQ_nb"]["y"][SNR÷5+3] = h5read(metrics_file, "new_PESQnb")
+        metrics_gs_sum["PESQ_nb"]["x"][SNR÷5+3] = SNR
+        metrics_gs_sum["PESQ_wb"]["y"][SNR÷5+3] = h5read(metrics_file, "new_PESQwb")
+        metrics_gs_sum["PESQ_wb"]["x"][SNR÷5+3] = SNR
+        metrics_gs_sum["STOI"]["y"][SNR÷5+3] = h5read(metrics_file, "new_STOI")
+        metrics_gs_sum["STOI"]["x"][SNR÷5+3] = SNR
+    end
+
+    # fetch metrics wiener
+    metrics_wiener = Dict("SNR"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "PESQ_nb"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "PESQ_wb"=> Dict("x"=> zeros(5), "y"=> zeros(5)), "STOI"=> Dict("x"=> zeros(5), "y"=> zeros(5)))
+    for metrics_file in filter(x -> occursin(".h5", a), readdir("paper/experiment1_clapping_SNR/exports/wiener",join=true))
+        SNR = -parse(Int64, split(metrics_file[findfirst("power=", metrics_file)[end]+1:end], ".")[1])
+        metrics_wiener["SNR"]["y"][SNR÷5+3] = h5read(metrics_file, "new_SNR")
+        metrics_wiener["SNR"]["x"][SNR÷5+3] = SNR
+        metrics_wiener["PESQ_nb"]["y"][SNR÷5+3] = h5read(metrics_file, "new_PESQnb")
+        metrics_wiener["PESQ_nb"]["x"][SNR÷5+3] = SNR
+        metrics_wiener["PESQ_wb"]["y"][SNR÷5+3] = h5read(metrics_file, "new_PESQwb")
+        metrics_wiener["PESQ_wb"]["x"][SNR÷5+3] = SNR
+        metrics_wiener["STOI"]["y"][SNR÷5+3] = h5read(metrics_file, "new_STOI")
+        metrics_wiener["STOI"]["x"][SNR÷5+3] = SNR
+    end
+end
+
+metrics_algonquin
+metrics_baseline
+metrics_gs_sum
+metrics_wiener
+
+# plt_metrics = @pgf Axis({   xlabel="input SNR",
+#                             ylabel="Free energy [nats]",
+#                             legend_pos = "north east",
+#                             legend_cell_align="{left}",
+#                             scale = 1.0,
+#                             grid = "major",
+#                             title = "FE $(nmixtures) mixtures",
+#                             },
+#                             Plot({no_marks, color="red"}, Coordinates(collect(2:niterations), em_fe[2:end])), LegendEntry("EM"),
+#                             Plot({no_marks, style ="{dashed}", color="blue"}, Coordinates(collect(2:niterations), vi_fe[2:end])), LegendEntry("VI"))
+
+# pgfsave("results/countries/plt_fe.tikz", plt_fe)
+
+
+plt_metrics = @pgf GroupPlot(
+    # group plot options
+    {
+        group_style = {
+            group_size="3 by 1",
+        },
+        no_markers,
+    },
+
+    # axis 1 (SNR)
+    {
+        xlabel="input SNR",
+        ylabel="output SNR",
+        grid = "major",
+    },
+    # plots for axis 1
+    Plot(Table(metrics_baseline["SNR"]["x"], metrics_baseline["SNR"]["y"])), LegendEntry("Baseline"),
+    Plot(Table(metrics_algonquin["SNR"]["x"], metrics_algonquin["SNR"]["y"])), LegendEntry("Algonquin"),
+    Plot(Table(metrics_gs_sum["SNR"]["x"], metrics_gs_sum["SNR"]["y"])), LegendEntry("GS sum"),
+    Plot(Table(metrics_wiener["SNR"]["x"], metrics_wiener["SNR"]["y"])), LegendEntry("Wiener"),
+    
+    # axis 2 (PESQ)
+    {
+        xlabel="input SNR",
+        ylabel="output PESQ",
+        grid = "major",
+    },
+    # plots for axis 2
+    Plot(Table(metrics_baseline["PESQwb"]["x"], metrics_baseline["PESQwb"]["y"])), LegendEntry("Baseline"),
+    Plot(Table(metrics_algonquin["PESQwb"]["x"], metrics_algonquin["PESQwb"]["y"])), LegendEntry("Algonquin"),
+    Plot(Table(metrics_gs_sum["PESQwb"]["x"], metrics_gs_sum["PESQwb"]["y"])), LegendEntry("GS sum"),
+    Plot(Table(metrics_wiener["PESQwb"]["x"], metrics_wiener["PESQwb"]["y"])), LegendEntry("Wiener"),
+
+    # axis 3 (STOI)
+    { 
+        xlabel="input SNR",
+        ylabel="output STOI",
+        grid = "major",
+    },
+    # plots for axis 3
+    Plot(Table(metrics_baseline["STOI"]["x"], metrics_baseline["STOI"]["y"])), LegendEntry("Baseline"),
+    Plot(Table(metrics_algonquin["STOI"]["x"], metrics_algonquin["STOI"]["y"])), LegendEntry("Algonquin"),
+    Plot(Table(metrics_gs_sum["STOI"]["x"], metrics_gs_sum["STOI"]["y"])), LegendEntry("GS sum"),
+    Plot(Table(metrics_wiener["STOI"]["x"], metrics_wiener["STOI"]["y"])), LegendEntry("Wiener"),
+
+)
+
+pgfsave("paper/experiment1_clapping_SNR/exports/figures/metrics.tikz", plt_metrics)
+
+metrics_algonquin
