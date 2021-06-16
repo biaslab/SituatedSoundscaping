8c343a1cfc4eba3853f26ae016ee3499355986e7
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-06-16 08:47:51 +0200

    update experiments

--- paper/experiment2_clapping_mixs/experiment2.jl
+++ paper/experiment2_clapping_mixs/experiment2.jl
@@ -27,7 +27,7 @@ begin
         
         # skip if already done
         if !overwrite 
-            if isfile("paper/experiment1_clapping_SNR/exports/algonquin_vb/metrics"*id*".txt")
+            if isfile("paper/experiment2_clapping_mixs/exports/algonquin_vb/metrics"*id*".txt")
                 continue
             end
         end
@@ -38,20 +38,20 @@ begin
         mixed_signal, speech_signal, noise_signal = create_mixture_signal("data/recorded_speech_raw/recording_speech.flac", "data/recorded_noise_raw/recording_noise.wav", duration_adapt=3.0, duration_test=10.0, power_noise_dB=ndB)
 
         # train speech mode
-        centers_speech, πk1_speech = train_kmeans("paper/experiment1_clapping_SNR/exports/models/Kmeans_speech", data_speech, nmix_speech)
-        means_speech, covs_speech, πk2_speech = train_em("paper/experiment1_clapping_SNR/exports/models/EM_speech", data_speech, centers_speech, πk1_speech; nr_iterations=nr_iterations_em)
-        q_μ_speech, q_γ_speech, q_a_speech = train_vb("paper/experiment1_clapping_SNR/exports/models/VB_speech", data_speech, means_speech, covs_speech, πk2_speech; nr_iterations=nr_iterations_vb);
+        centers_speech, πk1_speech = train_kmeans("paper/experiment2_clapping_mixs/exports/models/Kmeans_speech", data_speech, nmix_speech)
+        means_speech, covs_speech, πk2_speech = train_em("paper/experiment2_clapping_mixs/exports/models/EM_speech", data_speech, centers_speech, πk1_speech; nr_iterations=nr_iterations_em)
+        q_μ_speech, q_γ_speech, q_a_speech = train_vb("paper/experiment2_clapping_mixs/exports/models/VB_speech", data_speech, means_speech, covs_speech, πk2_speech; nr_iterations=nr_iterations_vb);
 
         # train noise model on recording
-        centers_noise, πk1_noise = train_kmeans("paper/experiment1_clapping_SNR/exports/models/Kmeans_noise", log.(abs2.(recording_noise)), nmix_noise; power_dB=ndB)
-        means_noise, covs_noise, πk2_noise = train_em("paper/experiment1_clapping_SNR/exports/models/EM_noise", log.(abs2.(recording_noise)), centers_noise, πk1_noise; nr_iterations=nr_iterations_em, power_dB=ndB)
-        q_μ_noise, q_γ_noise, q_a_noise = train_vb("paper/experiment1_clapping_SNR/exports/models/VB_noise", log.(abs2.(recording_noise)), means_noise, covs_noise, πk2_noise; nr_iterations=nr_iterations_vb, power_dB=ndB);
+        centers_noise, πk1_noise = train_kmeans("paper/experiment2_clapping_mixs/exports/models/Kmeans_noise", log.(abs2.(recording_noise)), nmix_noise; power_dB=ndB)
+        means_noise, covs_noise, πk2_noise = train_em("paper/experiment2_clapping_mixs/exports/models/EM_noise", log.(abs2.(recording_noise)), centers_noise, πk1_noise; nr_iterations=nr_iterations_em, power_dB=ndB)
+        q_μ_noise, q_γ_noise, q_a_noise = train_vb("paper/experiment2_clapping_mixs/exports/models/VB_noise", log.(abs2.(recording_noise)), means_noise, covs_noise, πk2_noise; nr_iterations=nr_iterations_vb, power_dB=ndB);
 
         # perform source separation
-        speech_sep = separate_sources_algonquin_vb("paper/experiment1_clapping_SNR/exports/algonquin_vb", mixed_signal, q_μ_speech, q_γ_speech, q_a_speech, q_μ_noise, q_γ_noise, q_a_noise; observation_noise_precision=observation_noise_precision, block_length=32, power_dB=ndB, save_results=true, nr_iterations=nr_iterations_vmp)
+        speech_sep = separate_sources_algonquin_vb("paper/experiment2_clapping_mixs/exports/algonquin_vb", mixed_signal, q_μ_speech, q_γ_speech, q_a_speech, q_μ_noise, q_γ_noise, q_a_noise; observation_noise_precision=observation_noise_precision, block_length=32, power_dB=ndB, save_results=true, nr_iterations=nr_iterations_vmp)
 
         # evaluate metrics
-        evaluate_metrics("paper/experiment1_clapping_SNR/exports/algonquin_vb/metrics"*id*".txt", speech_sep, mixed_signal, speech_signal)
+        evaluate_metrics("paper/experiment2_clapping_mixs/exports/algonquin_vb/metrics"*id*".txt", speech_sep, mixed_signal, speech_signal)
         
     end
 end
@@ -83,7 +83,7 @@ begin
 
         # skip if already done
         if !overwrite 
-            if isfile("paper/experiment1_clapping_SNR/exports/gs_sum/metrics"*id*".txt")
+            if isfile("paper/experiment2_clapping_mixs/exports/gs_sum/metrics"*id*".txt")
                 continue
             end
         end
@@ -94,20 +94,20 @@ begin
         mixed_signal, speech_signal, noise_signal = create_mixture_signal("data/recorded_speech_raw/recording_speech.flac", "data/recorded_noise_raw/recording_noise.wav", duration_adapt=3.0, duration_test=10.0, power_noise_dB=ndB)
 
         # train model speech
-        centers_speech, πk1_speech = train_kmeans("paper/experiment1_clapping_SNR/exports/models/Kmeans_speech", data_speech, nmix_speech)
-        means_speech, covs_speech, πk2_speech = train_em("paper/experiment1_clapping_SNR/exports/models/EM_speech", data_speech, centers_speech, πk1_speech; nr_iterations=nr_iterations_em)
-        q_μ_speech, q_γ_speech, q_a_speech = train_gs("paper/experiment1_clapping_SNR/exports/models/GS_speech", data_speech, means_speech, covs_speech, πk2_speech; nr_iterations=nr_iterations_gs);
+        centers_speech, πk1_speech = train_kmeans("paper/experiment2_clapping_mixs/exports/models/Kmeans_speech", data_speech, nmix_speech)
+        means_speech, covs_speech, πk2_speech = train_em("paper/experiment2_clapping_mixs/exports/models/EM_speech", data_speech, centers_speech, πk1_speech; nr_iterations=nr_iterations_em)
+        q_μ_speech, q_γ_speech, q_a_speech = train_gs("paper/experiment2_clapping_mixs/exports/models/GS_speech", data_speech, means_speech, covs_speech, πk2_speech; nr_iterations=nr_iterations_gs);
 
         # train noise model on recording
-        centers_noise, πk1_noise = train_kmeans("paper/experiment1_clapping_SNR/exports/models/Kmeans_noise", log.(abs2.(recording_noise)), nmix_noise; power_dB=ndB)
-        means_noise, covs_noise, πk2_noise = train_em("paper/experiment1_clapping_SNR/exports/models/EM_noise", log.(abs2.(recording_noise)), centers_noise, πk1_noise; nr_iterations=nr_iterations_em, power_dB=ndB)
-        q_μ_noise, q_γ_noise, q_a_noise = train_gs("paper/experiment1_clapping_SNR/exports/models/GS_noise", recording_noise, means_noise, covs_noise, πk2_noise; nr_iterations=nr_iterations_gs, power_dB=ndB);
+        centers_noise, πk1_noise = train_kmeans("paper/experiment2_clapping_mixs/exports/models/Kmeans_noise", log.(abs2.(recording_noise)), nmix_noise; power_dB=ndB)
+        means_noise, covs_noise, πk2_noise = train_em("paper/experiment2_clapping_mixs/exports/models/EM_noise", log.(abs2.(recording_noise)), centers_noise, πk1_noise; nr_iterations=nr_iterations_em, power_dB=ndB)
+        q_μ_noise, q_γ_noise, q_a_noise = train_gs("paper/experiment2_clapping_mixs/exports/models/GS_noise", recording_noise, means_noise, covs_noise, πk2_noise; nr_iterations=nr_iterations_gs, power_dB=ndB);
 
         # perform source separation
-        speech_sep = separate_sources_gs_sum("paper/experiment1_clapping_SNR/exports/gs_sum", mixed_signal, q_μ_speech, q_γ_speech, q_a_speech, q_μ_noise, q_γ_noise, q_a_noise; block_length=32, power_dB=ndB, save_results=true, nr_iterations=nr_iterations_gs)
+        speech_sep = separate_sources_gs_sum("paper/experiment2_clapping_mixs/exports/gs_sum", mixed_signal, q_μ_speech, q_γ_speech, q_a_speech, q_μ_noise, q_γ_noise, q_a_noise; block_length=32, power_dB=ndB, save_results=true, nr_iterations=nr_iterations_gs)
 
         # evaluate metrics
-        evaluate_metrics("paper/experiment1_clapping_SNR/exports/gs_sum/metrics"*id*".txt", speech_sep, mixed_signal, speech_signal)
+        evaluate_metrics("paper/experiment2_clapping_mixs/exports/gs_sum/metrics"*id*".txt", speech_sep, mixed_signal, speech_signal)
 
     end
 end
@@ -130,10 +130,10 @@ begin
         mixed_signal, speech_signal, noise_signal = create_mixture_signal("data/recorded_speech_raw/recording_speech.flac", "data/recorded_noise_raw/recording_noise.wav", duration_adapt=3.0, duration_test=10.0, power_noise_dB=ndB)
 
         # separate sources
-        speech_sep, _, _, _, _ = separate_sources_wiener("paper/experiment1_clapping_SNR/exports/wiener", mixed_signal, speech_signal, noise_signal; block_length=32, power_dB=ndB, save_results=true)
+        speech_sep, _, _, _, _ = separate_sources_wiener("paper/experiment2_clapping_mixs/exports/wiener", mixed_signal, speech_signal, noise_signal; block_length=32, power_dB=ndB, save_results=true)
 
         # evaluate metrics
-        evaluate_metrics("paper/experiment1_clapping_SNR/exports/wiener/metrics"*id*".txt", speech_sep, mixed_signal, speech_signal)
+        evaluate_metrics("paper/experiment2_clapping_mixs/exports/wiener/metrics"*id*".txt", speech_sep, mixed_signal, speech_signal)
 
     end
 end
