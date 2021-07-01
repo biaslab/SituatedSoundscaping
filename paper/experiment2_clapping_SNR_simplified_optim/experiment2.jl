7c448dc3398210428dffc8a8b9c8a43f639c3c80
Bart van Erp <b.v.erp@tue.nl>
2021-07-01 10:07:48 +0200

    update experiments

---
+++ paper/experiment2_clapping_SNR_simplified_optim/experiment2.jl
@@ -0,0 +1,139 @@
+using SituatedSoundscaping
+
+
+# experiments algonquin_vb_simplified
+begin 
+    # settings
+    nr_mixtures_speech = [25]
+    nr_mixtures_noise = [2]
+    nr_files_speech = 1000
+    nr_iterations_em = 10
+    nr_iterations_vb = 10
+    nr_iterations_vmp = 10
+    observation_noise_precision = 10.0
+    power_noise = [-10, -5, 0, 5, 10]
+    overwrite = false
+
+    # perform experiments
+    data_speech = prepare_data("data/train_speech_raw", "data/train_speech_processed32"; block_length=32)
+    nr_files_speech = minimum([nr_files_speech, length(data_speech)])
+    data_speech = data_speech[1:nr_files_speech]
+
+    for (ndB, nmix_speech, nmix_noise) in Iterators.product(power_noise, nr_mixtures_speech, nr_mixtures_noise)
+
+        # create identifier
+        nr_freqs = 32 ÷ 2 + 1
+        id = string("_freq=", nr_freqs, "_mixs=", nmix_speech, "_mixn=", nmix_noise, "_power=", ndB)
+        
+        # skip if already done
+        if !overwrite 
+            if isfile("paper/experiment2_clapping_SNR_simplified_optim/exports/algonquin_vb/metrics"*id*".txt")
+                continue
+            end
+        end
+
+        # prepare data sets
+        prepare_data("data/recorded_noise_raw", "data/recorded_noise_processed32/"*string(ndB); block_length=32, power_dB=ndB)
+        recording_noise = read_recording("data/recorded_noise_processed32/"*string(ndB)*"/0000000001.h5", duration=3)
+        mixed_signal, speech_signal, noise_signal = create_mixture_signal("data/recorded_speech_raw/recording_speech.flac", "data/recorded_noise_raw/recording_noise.wav", duration_adapt=3.0, duration_test=10.0, power_noise_dB=ndB)
+
+        # train speech mode
+        centers_speech, πk1_speech = train_kmeans("paper/experiment2_clapping_SNR_simplified_optim/exports/models/Kmeans_speech", data_speech, nmix_speech)
+        means_speech, covs_speech, πk2_speech = train_em("paper/experiment2_clapping_SNR_simplified_optim/exports/models/EM_speech", data_speech, centers_speech, πk1_speech; nr_iterations=nr_iterations_em)
+        q_μ_speech, q_γ_speech, q_a_speech = train_vb("paper/experiment2_clapping_SNR_simplified_optim/exports/models/VB_speech", data_speech, means_speech, covs_speech, πk2_speech; nr_iterations=nr_iterations_vb);
+
+        # train noise model on recording
+        centers_noise, πk1_noise = train_kmeans("paper/experiment2_clapping_SNR_simplified_optim/exports/models/Kmeans_noise", log.(abs2.(recording_noise)), nmix_noise; power_dB=ndB)
+        means_noise, covs_noise, πk2_noise = train_em("paper/experiment2_clapping_SNR_simplified_optim/exports/models/EM_noise", log.(abs2.(recording_noise)), centers_noise, πk1_noise; nr_iterations=nr_iterations_em, power_dB=ndB)
+        q_μ_noise, q_γ_noise, q_a_noise = train_vb("paper/experiment2_clapping_SNR_simplified_optim/exports/models/VB_noise", log.(abs2.(recording_noise)), means_noise, covs_noise, πk2_noise; nr_iterations=nr_iterations_vb, power_dB=ndB);
+
+        # perform source separation
+        speech_sep = separate_sources_algonquin_vb_simplified("paper/experiment2_clapping_SNR_simplified_optim/exports/algonquin_vb", mixed_signal, q_μ_speech, q_γ_speech, q_a_speech, q_μ_noise, q_γ_noise, q_a_noise; observation_noise_precision=observation_noise_precision, block_length=32, power_dB=ndB, save_results=true, nr_iterations=nr_iterations_vmp)
+        
+        # evaluate metrics
+        evaluate_metrics_optim("paper/experiment2_clapping_SNR_simplified_optim/exports/algonquin_vb/metrics"*id*".txt", speech_sep, mixed_signal, speech_signal)
+        
+    end
+end
+# END:  experiments algonquin_vb_simplified
+
+
+# experiments gs_sum
+begin
+        
+    # settings
+    nr_mixtures_speech = [25]
+    nr_mixtures_noise = [2]
+    nr_files_speech = 1000
+    nr_iterations_em = 10
+    nr_iterations_gs = 10
+    power_noise = [-10, -5, 0, 5, 10]
+    overwrite = false
+
+    # perform experiments
+    data_speech = prepare_data("data/train_speech_raw", "data/train_speech_processed32"; block_length=32)
+    nr_files_speech = minimum([nr_files_speech, length(data_speech)])
+    data_speech = data_speech[1:nr_files_speech]
+
+    for (ndB, nmix_speech, nmix_noise) in Iterators.product(power_noise, nr_mixtures_speech, nr_mixtures_noise)
+
+        # create identifier
+        nr_freqs = 32 ÷ 2 + 1
+        id = string("_freq=", nr_freqs, "_mixs=", nmix_speech, "_mixn=", nmix_noise, "_power=", ndB)
+
+        # skip if already done
+        if !overwrite 
+            if isfile("paper/experiment2_clapping_SNR_simplified_optim/exports/gs_sum/metrics"*id*".txt")
+                continue
+            end
+        end
+
+        # prepare data sets
+        prepare_data("data/recorded_noise_raw", "data/recorded_noise_processed32/"*string(ndB); block_length=32, power_dB=ndB)
+        recording_noise = read_recording("data/recorded_noise_processed32/"*string(ndB)*"/0000000001.h5", duration=3)
+        mixed_signal, speech_signal, noise_signal = create_mixture_signal("data/recorded_speech_raw/recording_speech.flac", "data/recorded_noise_raw/recording_noise.wav", duration_adapt=3.0, duration_test=10.0, power_noise_dB=ndB)
+
+        # train model speech
+        centers_speech, πk1_speech = train_kmeans("paper/experiment2_clapping_SNR_simplified_optim/exports/models/Kmeans_speech", data_speech, nmix_speech)
+        means_speech, covs_speech, πk2_speech = train_em("paper/experiment2_clapping_SNR_simplified_optim/exports/models/EM_speech", data_speech, centers_speech, πk1_speech; nr_iterations=nr_iterations_em)
+        q_μ_speech, q_γ_speech, q_a_speech = train_gs("paper/experiment2_clapping_SNR_simplified_optim/exports/models/GS_speech", data_speech, means_speech, covs_speech, πk2_speech; nr_iterations=nr_iterations_gs);
+
+        # train noise model on recording
+        centers_noise, πk1_noise = train_kmeans("paper/experiment2_clapping_SNR_simplified_optim/exports/models/Kmeans_noise", log.(abs2.(recording_noise)), nmix_noise; power_dB=ndB)
+        means_noise, covs_noise, πk2_noise = train_em("paper/experiment2_clapping_SNR_simplified_optim/exports/models/EM_noise", log.(abs2.(recording_noise)), centers_noise, πk1_noise; nr_iterations=nr_iterations_em, power_dB=ndB)
+        q_μ_noise, q_γ_noise, q_a_noise = train_gs("paper/experiment2_clapping_SNR_simplified_optim/exports/models/GS_noise", recording_noise, means_noise, covs_noise, πk2_noise; nr_iterations=nr_iterations_gs, power_dB=ndB);
+
+        # perform source separation
+        speech_sep = separate_sources_gs_sum_simplified("paper/experiment2_clapping_SNR_simplified_optim/exports/gs_sum", mixed_signal, q_μ_speech, q_γ_speech, q_a_speech, q_μ_noise, q_γ_noise, q_a_noise; block_length=32, power_dB=ndB, save_results=true, nr_iterations=nr_iterations_gs)
+
+        # evaluate metrics
+        evaluate_metrics_optim("paper/experiment2_clapping_SNR_simplified_optim/exports/gs_sum/metrics"*id*".txt", speech_sep, mixed_signal, speech_signal)
+
+    end
+end
+# END: experiments gs_sum
+
+
+# experiments Wiener
+begin
+    
+    # settings
+    power_noise = [-10, -5, 0, 5, 10]
+
+    # perform experiments
+    for ndB in power_noise
+
+        # prepare id
+        id = string("_power=", ndB)
+
+        # prepare data
+        mixed_signal, speech_signal, noise_signal = create_mixture_signal("data/recorded_speech_raw/recording_speech.flac", "data/recorded_noise_raw/recording_noise.wav", duration_adapt=3.0, duration_test=10.0, power_noise_dB=ndB)
+
+        # separate sources
+        speech_sep, _, _, _, _ = separate_sources_wiener("paper/experiment2_clapping_SNR_simplified_optim/exports/wiener", mixed_signal, speech_signal, noise_signal; block_length=32, power_dB=ndB, save_results=true)
+
+        # evaluate metrics
+        evaluate_metrics("paper/experiment2_clapping_SNR_simplified_optim/exports/wiener/metrics"*id*".txt", speech_sep, mixed_signal, speech_signal)
+
+    end
+end
--- src/metrics/metrics.jl
+++ src/metrics/metrics.jl
@@ -1,7 +1,7 @@
 using PyCall
 using Conda
 
-export SNR, __init__, PESQ, STOI, evaluate_metrics
+export SNR, __init__, PESQ, STOI, evaluate_metrics, evaluate_metrics_optim
 
 # install conda packages
 Conda.pip_interop(true)
@@ -21,6 +21,81 @@ function __init__()
     copy!(PESQ, pyimport("pesq").pesq)
     copy!(STOI, pyimport("pystoi").stoi)
 end
+
+function evaluate_metrics_optim(filename, speech_sep, speech_noisy, speech_true; block_length=32::Int, fs=16000::Real)
+
+    # also 'warp' the original signal
+    filterbank_true = warped_filter_bank(block_duration_s=block_length/fs, nr_bands=Int(block_length/2)+1)
+    filterbank_noisy = warped_filter_bank(block_duration_s=block_length/fs, nr_bands=Int(block_length/2)+1)
+    nr_blocks = Int(length(speech_true)/block_length)
+    speech_true_warped = zeros(size(speech_true))
+    speech_noisy_warped = zeros(size(speech_noisy))
+    for k in 1:nr_blocks
+
+        # feed signals into filterbanks
+        run!(filterbank_true, speech_true[1+(k-1)*block_length:k*block_length])
+        run!(filterbank_noisy, speech_noisy[1+(k-1)*block_length:k*block_length])
+
+        # read from filter
+        speech_true_warped[1+(k-1)*block_length:k*block_length] = read(filterbank_true)
+        speech_noisy_warped[1+(k-1)*block_length:k*block_length] = read(filterbank_noisy)
+
+    end
+
+    # calculate metrics
+    SNRo = zeros(Float64, 100)
+    pesqw = zeros(Float64, 100)
+    pesqn = zeros(Float64, 100)
+    stoi = zeros(Float64, 100)
+    for k = 1:100
+        SNRo[k] = SNR(speech_true_warped, k/100*speech_sep + (100-k)/100*(speech_noisy - speech_sep))
+        pesqw[k] = PESQ(fs, speech_true_warped, k/100*speech_sep + (100-k)/100*(speech_noisy - speech_sep), "wb")
+        pesqn[k] = PESQ(fs, speech_true_warped, k/100*speech_sep + (100-k)/100*(speech_noisy - speech_sep), "nb")
+        stoi[k] = STOI(speech_true_warped, k/100*speech_sep + (100-k)/100*(speech_noisy - speech_sep), fs, extended=false)
+    end
+    SNRo = maximum(SNRo)
+    pesqw = maximum(pesqw)
+    pesqn = maximum(pesqn)
+    stoi = maximum(stoi)
+
+    # calculate reference metrics
+    SNR_baseline = SNR(speech_true_warped, speech_noisy_warped)
+    pesqw_baseline = PESQ(fs, speech_true_warped, speech_noisy_warped, "wb")
+    pesqn_baseline = PESQ(fs, speech_true_warped, speech_noisy_warped, "nb")
+    stoi_baseline = STOI(speech_true_warped, speech_noisy_warped, fs, extended=false)
+
+    # save metrics
+    open(filename, "w") do f
+        write(f, "BASELINE \n")
+        write(f, "-------- \n")
+        write(f, "SNR = $SNR_baseline \n")
+        write(f, "PESQ (wb)= $pesqw_baseline \n")
+        write(f, "PESQ (nb)= $pesqn_baseline \n")
+        write(f, "STOI= $stoi_baseline \n")
+        write(f, "\n")
+        write(f, "\n")
+        write(f, "RESULTS \n")
+        write(f, "-------- \n")
+        write(f, "SNR = $SNRo \n")
+        write(f, "PESQ (wb)= $pesqw \n")
+        write(f, "PESQ (nb)= $pesqn \n")
+        write(f, "STOI= $stoi \n")
+    end
+
+    # save metrics
+    f = h5open(filename[1:end-3]*"h5", "w")
+    HDF5.write(f, "baseline_SNR", SNR_baseline);
+    HDF5.write(f, "baseline_PESQwb", pesqw_baseline);
+    HDF5.write(f, "baseline_PESQnb", pesqn_baseline);
+    HDF5.write(f, "baseline_STOI", stoi_baseline);
+    HDF5.write(f, "new_SNR", SNRo);
+    HDF5.write(f, "new_PESQwb", pesqw);
+    HDF5.write(f, "new_PESQnb", pesqn);
+    HDF5.write(f, "new_STOI", stoi);
+    close(f)
+end
+
+
 
 function evaluate_metrics(filename, speech_sep, speech_noisy, speech_true; block_length=32::Int, fs=16000::Real)
 
